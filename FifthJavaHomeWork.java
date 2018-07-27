4034878cf9a80d22421da8ae20e06c2729863297
ViktorSever <35972664+ViktorSever@users.noreply.github.com>
2018-07-27 12:02:02 +0300

    Update FifthJavaHomeWork.java

--- FifthJavaHomeWork.java
+++ FifthJavaHomeWork.java
@@ -1,61 +1,81 @@
-import java.util.Arrays;
 /**
  * author Viktor Shkuratov
- * date 26/07/2018
+ * date 27/07/2018
  * Java 1 HomeWork 5
  */
-public class Person {
+public class JavaFifthHomeWork {
+    private Person[] personArr;
+    public static void main (String[] args) {
+        JavaFifthHomeWork javaFifthHomeWork = new JavaFifthHomeWork();
+        javaFifthHomeWork.firstTask(javaFifthHomeWork.getPersonArr());
+    }
 
-    String surname;
-    String name;
-    String secondName;
-    String position;
-    String eMail;
-    String number;
-    int salary;
-    int age;
+    public JavaFifthHomeWork() {
+        personArr = initPersons();
+    }
 
-    public static void main(String[] args) {
-        Person [] personArr = new Person[5];
-        personArr [0] = new Person("Ivanov", "Ivan", "Ivanovich","java-developer",
+    private Person[] getPersonArr() {
+        return personArr;
+    }
+
+    private Person[] initPersons() {
+        Person [] people = new Person[5];
+        people [0] = new Person("Ivanov", "Ivan", "Ivanovich","java-developer",
                 "i@gmail.com","88005553535",100000,35);
 
-        personArr [1] = new Person("Ivanov", "Petr", "Ivanovich","java-developer",
+        people [1] = new Person("Ivanov", "Petr", "Ivanovich","java-developer",
                 "i@gmail.com","88005553535",100000,20);
 
-        personArr [2] = new Person("Sever", "Olga", "Ivanovna","java-developer",
+        people [2] = new Person("Sever", "Olga", "Ivanovna","java-developer",
                 "i@gmail.com","88005553535",100000,26);
 
-       personArr [3] = new Person("Malkina", "Nadeshda", "Petrovna","java-developer",
+        people [3] = new Person("Malkina", "Nadeshda", "Petrovna","java-developer",
                 "i@gmail.com","88005553535",100000,45);
 
-        personArr [4] = new Person("Yhin", "Oleg", "Petrovich","java-developer",
+        people [4] = new Person("Yhin", "Oleg", "Petrovich","java-developer",
                 "i@gmail.com","88005553535",100000,50);
+        return people;
+    }
 
+    public void firstTask(Person [] personArr) {
         for(Person person:personArr) {
-            if (person.age > 40) {
+            if (person.getAge() > 40) {
                 System.out.println();
                 System.out.println("Person:\n"+person.toString());
             }
         }
     }
 
-    public Person(String surname, String name, String secondName, String position, String eMail, String number,
-                  int salary, int age) {
-        this.surname = surname;
-        this.name = name;
-        this.secondName = secondName;
-        this.position = position;
-        this.eMail = eMail;
-        this.number = number;
-        this.salary = salary;
-        this.age = age;
-    }
+    public class Person {
+        private String surname;
+        private String name;
+        private String secondName;
+        private String position;
+        private String eMail;
+        private String number;
+        private int salary;
+        private int age;
 
-    @Override
-    public String toString() {
-        return "Surname: " + surname + "\nName: " + name + "\nSecondName: " + secondName + "\nPosition: " + position +
-                "\ne-Mail: " + eMail + "\nPhone Number: " + number + "\nSalary: " + salary + "\nAge: "
-                + age;
+        public Person(String surname, String name, String secondName, String position, String eMail, String number, int salary, int age) {
+            this.surname = surname;
+            this.name = name;
+            this.secondName = secondName;
+            this.position = position;
+            this.eMail = eMail;
+            this.number = number;
+            this.salary = salary;
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return "Surname: " + surname + "\nName: " + name + "\nSecondName: " + secondName + "\nPosition: " + position +
+                    "\ne-Mail: " + eMail + "\nPhone Number: " + number + "\nSalary: " + salary + "\nAge: "
+                    + age;
+        }
     }
 }
